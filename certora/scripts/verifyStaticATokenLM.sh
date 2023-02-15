a1bb5d09e51ea545729c1194d3d59e9749d2ac93
gadicer <gadi.auerbch@certora.com>
2023-02-15 17:28:24 +0200

    add link to erc20

--- certora/scripts/verifyStaticATokenLM.sh
+++ certora/scripts/verifyStaticATokenLM.sh
@@ -5,11 +5,15 @@ certoraRun src/StaticATokenLM.sol \
     lib/aave-v3-core/contracts/protocol/tokenization/AToken.sol \
     certora/harness/RewardsDistributorHarness.sol \
     certora/harness/TransferStrategyHarness.sol \
-    certora/harness/DummyERC20A.sol certora/harness/DummyERC20B.sol \
+    certora/harness/DummyERC20_aTokenUnderlying.sol \
+    certora/harness/DummyERC20_rewardToken.sol \
     --verify StaticATokenLM:certora/specs/StaticATokenLM.spec \
     --link StaticATokenLM:_incentivesController=RewardsController \
            StaticATokenLM:_pool=SymbolicLendingPoolL1 \
            AToken:POOL=SymbolicLendingPoolL1 \
+            StaticATokenLM:_aToken=AToken \
+           StaticATokenLM:_aTokenUnderlying=DummyERC20_aTokenUnderlying \
+           StaticATokenLM:_rewardToken=DummyERC20_rewardToken \
     --solc solc8.10 \
     --optimistic_loop \
     --staging \
@@ -18,8 +22,12 @@ certoraRun src/StaticATokenLM.sol \
                aave-v3-periphery=lib/aave-v3-periphery \
                solidity-utils=lib/solidity-utils/src \
     --send_only \
-    --msg "StaticATokenLM rules remove functions from RewardsController.sol"
+    --msg "aToken=AToken _rewardToken _aTokenUnderlying.  "
 
+#link _aToken _aTokenUnderlying _aToken
+#           StaticATokenLM:_aToken=DummyERC20_aToken \
+#           StaticATokenLM:_aTokenUnderlying=DummyERC20_aTokenUnderlying \
+#           StaticATokenLM:_rewardToken=DummyERC20_rewardToken \
 #    certora/harness/ScaledBalanceTokenHarness.sol \
 #    lib/aave-v3-periphery/contracts/rewards/transfer-strategies/StakedTokenTransferStrategy.sol \
 #    lib/aave-v3-periphery/contracts/rewards/transfer-strategies/PullRewardsTransferStrategy.sol \
