f6b8db3dee548f2b6e045c35e3844bb74bcae63e
Michael M <MichaelM@certora.com>
2023-03-14 19:46:03 +0200

    fixing harnesses and script

--- certora/scripts/verifyStaticATokenLM.sh
+++ certora/scripts/verifyStaticATokenLM.sh
@@ -2,7 +2,7 @@
 certoraRun  certora/harness/StaticATokenLMHarness.sol \
     certora/harness/RewardsControllerHarness.sol \
     certora/harness/SymbolicLendingPoolL1.sol \
-    lib/aave-v3-core/contracts/protocol/tokenization/AToken.sol \
+    certora/munged/lib/aave-v3-core/contracts/protocol/tokenization/AToken.sol \
     certora/harness/TransferStrategyHarness.sol \
     certora/harness/ScaledBalanceTokenHarness.sol \
     certora/harness/DummyERC20_aTokenUnderlying.sol \
@@ -11,7 +11,7 @@ certoraRun  certora/harness/StaticATokenLMHarness.sol \
     --link StaticATokenLMHarness:INCENTIVES_CONTROLLER=RewardsControllerHarness \
            StaticATokenLMHarness:POOL=SymbolicLendingPoolL1 \
            AToken:POOL=SymbolicLendingPoolL1 \
-            StaticATokenLMHarness:_aToken=AToken \
+           StaticATokenLMHarness:_aToken=AToken \
            StaticATokenLMHarness:_aTokenUnderlying=DummyERC20_aTokenUnderlying \
            AToken:_incentivesController=RewardsControllerHarness \
     --solc solc8.10 \
