75160e91eb09ba44ec7c5a04ec7bdad061d3c32f
Michael M <MichaelM@certora.com>
2023-03-30 19:43:36 +0300

    change staging to cloud

--- certora/scripts/runComplexity.sh
+++ certora/scripts/runComplexity.sh
@@ -3,7 +3,7 @@ certoraRun src/StaticATokenLM.sol \
     --verify StaticATokenLM:certora/specs/complexity.spec \
     --solc solc8.10 \
     --optimistic_loop \
-    --staging \
+    --cloud \
     --packages aave-v3-core=lib/aave-v3-core \
                 aave-v3-periphery=lib/aave-v3-periphery \
                 solidity-utils/contracts=lib/solidity-utils/src/contracts \
