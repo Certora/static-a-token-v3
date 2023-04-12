617867c1e6be9752b4909a08a7b3f9a823f60f33
Michael M <MichaelM@certora.com>
2023-04-12 17:20:37 +0300

    removing if-else

--- certora/scripts/verifyERC4626.sh
+++ certora/scripts/verifyERC4626.sh
@@ -1,6 +1,4 @@
-if (($# > 0))
-then
-    certoraRun certora/harness/StaticATokenLMHarness.sol \
+certoraRun certora/harness/StaticATokenLMHarness.sol \
         certora/harness/pool/SymbolicLendingPool.sol \
         certora/munged/lib/aave-v3-core/contracts/protocol/tokenization/AToken.sol \
         certora/harness/rewards/RewardsControllerHarness.sol \
@@ -31,35 +29,3 @@ then
     --settings -t=10000 \
     --rules "${@}" \
     --msg "ERC4626 properties"
-else
-    certoraRun certora/harness/StaticATokenLMHarness.sol \
-        certora/harness/pool/SymbolicLendingPool.sol \
-        certora/munged/lib/aave-v3-core/contracts/protocol/tokenization/AToken.sol \
-        certora/harness/rewards/RewardsControllerHarness.sol \
-        certora/harness/rewards/TransferStrategyHarness.sol \
-        certora/harness/tokens/DummyERC20_aTokenUnderlying.sol \
-        certora/harness/tokens/DummyERC20_rewardToken.sol \
-    --verify StaticATokenLMHarness:certora/specs/erc4626.spec \
-    --link StaticATokenLMHarness:POOL=SymbolicLendingPool \
-            StaticATokenLMHarness:INCENTIVES_CONTROLLER=RewardsControllerHarness \
-            StaticATokenLMHarness:_aTokenUnderlying=DummyERC20_aTokenUnderlying \
-            StaticATokenLMHarness:_aToken=AToken \
-            SymbolicLendingPool:underlyingToken=DummyERC20_aTokenUnderlying \
-            SymbolicLendingPool:aToken=AToken \
-            TransferStrategyHarness:INCENTIVES_CONTROLLER=RewardsControllerHarness \
-            AToken:POOL=SymbolicLendingPool \
-            AToken:_incentivesController=RewardsControllerHarness \
-            AToken:_underlyingAsset=DummyERC20_aTokenUnderlying \
-    --packages aave-v3-core=certora/munged/lib/aave-v3-core \
-                @aave/core-v3=certora/munged/lib/aave-v3-core \
-                aave-v3-periphery=certora/munged/lib/aave-v3-periphery \
-                solidity-utils=certora/munged/lib/solidity-utils/src \
-                openzeppelin-contracts=certora/munged/lib/openzeppelin-contracts \
-    --solc solc8.10 \
-    --optimistic_loop \
-        --loop_iter 1 \
-    --optimistic_hashing \
-    --cloud \
-    --settings -t=10000 \
-    --msg "ERC4626 properties"
-fi
